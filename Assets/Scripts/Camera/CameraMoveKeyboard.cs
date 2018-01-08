5bf707ce46f8a85a8281e192bd0ad21037800986
Daniel Wayota <danielglez1990@gmail.com>
2018-01-08 20:11:53 +0000

    Episode_1

---
+++ Assets/Scripts/Camera/CameraMoveKeyboard.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CameraMoveKeyboard : MonoBehaviour
+{
+	public float speed = 2;
+
+	private Vector3 movement = Vector3.zero;
+	
+	// ================================
+	void Update()
+	{
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		if (horizontal != 0 || vertical != 0)
+		{
+			this.movement.Set(horizontal, 0, vertical);
+
+			this.transform.Translate( this.movement * this.speed * Time.deltaTime );
+		}
+	}
+}
---
+++ Assets/Scripts/Camera/CameraZoom.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+	public Camera sceneCamera;
+	public float minSize = 8;
+	public float maxSize = 16;
+
+	private float currentSize;
+
+	// ================================
+	void Start()
+	{
+		this.currentSize = this.sceneCamera.orthographicSize;
+	}
+	
+	// ================================
+	void Update()
+	{
+		if (Input.mouseScrollDelta.y != 0)
+		{
+			this.currentSize -= Input.mouseScrollDelta.y;
+
+			this.currentSize = Mathf.Clamp(this.currentSize, this.minSize, this.maxSize);
+
+			this.sceneCamera.orthographicSize = this.currentSize;
+		}
+	}
+}
