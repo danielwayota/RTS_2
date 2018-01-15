f4c711f4ee012f718619536f69402679c4ee79e2
Daniel Wayota <danielglez1990@gmail.com>
2018-01-15 19:56:45 +0000

    Episode_2

---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Unit : MonoBehaviour
+{
+	private NavMeshAgent agent;
+
+	// PROTOTYPE
+	public Camera sceneCamera;
+	private Plane groundPlane;
+	// END PROTOTYPE
+
+	// ================================
+	void Start()
+	{
+		this.agent = GetComponent<NavMeshAgent>();
+
+		this.groundPlane.SetNormalAndPosition(Vector3.up, Vector3.zero);
+	}
+	
+	// ================================
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(1))
+		{
+			// Calcular posicion de destino
+			Ray ray = sceneCamera.ScreenPointToRay(Input.mousePosition);
+			float distance;
+
+			groundPlane.Raycast(ray, out distance);
+			Vector3 point = ray.GetPoint(distance);
+
+			// Mover el agente
+			this.agent.SetDestination(point);
+		}
+	}
+}
