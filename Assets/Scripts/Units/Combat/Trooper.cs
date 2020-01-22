fa4302bdd1d726a3293bd82f3d77c0cc522fa934
Daniel Wayota <danielglez1990@gmail.com>
2020-01-22 19:28:00 +0000

    Better trooper vision

--- Assets/Scripts/Units/Combat/Trooper.cs
+++ Assets/Scripts/Units/Combat/Trooper.cs
@@ -6,20 +6,26 @@ public class Trooper : Unit
 {
     public LayerMask unitsLayer;
     public float maxCombatDistance = 10;
+    public float maxViewDistance = 20;
+    public float halfFieldOfView = 15;
 
     private float time;
-    private float timeOut = 1f;
+    private float timeOut = .25f;
 
     private Weapon weapon;
     private Walk walk;
 
     private Unit currentTarget;
 
+    private Quaternion vigilanceRotation;
+
     // ================================
     void Awake()
     {
         this.weapon = GetComponent<Weapon>();
         this.walk = GetComponent<Walk>();
+
+        this.vigilanceRotation = this.transform.rotation;
     }
 
     // ================================
@@ -35,23 +41,24 @@ public class Trooper : Unit
             // Combate
             if (this.currentTarget == null)
             {
-                this.CheckSurrounding();
-            }
-            else
-            {
-                float distance = Vector3.Distance(
-                    this.transform.position, this.currentTarget.transform.position
-                );
 
-                if (distance > this.maxCombatDistance)
+                if (this.walk.status == WalkStatus.IDLE)
                 {
-                    this.currentTarget = null;
+                    this.vigilanceRotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
                 }
             }
+            this.CheckSurrounding();
         }
 
 
-        if (this.currentTarget != null)
+        if (this.currentTarget == null)
+        {
+            if (this.walk.status == WalkStatus.IDLE)
+            {
+                this.transform.rotation = Quaternion.Lerp(this.transform.rotation, this.vigilanceRotation, Time.deltaTime);
+            }
+        }
+        else
         {
             Vector3 target = this.currentTarget.transform.position;
             this.transform.LookAt(target);
@@ -66,6 +73,8 @@ public class Trooper : Unit
     // =================================
     public void CheckSurrounding()
     {
+        this.currentTarget = null;
+
         // Comprobar los alrededores.
         Collider[] nearUnits = Physics.OverlapSphere(
             this.transform.position,
@@ -82,6 +91,38 @@ public class Trooper : Unit
                 if (posibleEnemy.faction != this.faction)
                 {
                     this.currentTarget = posibleEnemy;
+                }
+            }
+        }
+
+        if (this.currentTarget != null)
+        {
+            // Hay una cerca
+            return;
+        }
+
+        nearUnits = Physics.OverlapSphere(
+            this.transform.position,
+            this.maxViewDistance,
+            this.unitsLayer
+        );
+
+        for (int i = 0; i < nearUnits.Length; i++)
+        {
+            if (nearUnits[i].gameObject != this.gameObject)
+            {
+                Unit posibleEnemy = nearUnits[i].GetComponent<Unit>();
+
+                if (posibleEnemy.faction != this.faction)
+                {
+                    Vector3 toEnemy = (posibleEnemy.transform.position - this.transform.position).normalized;
+
+                    float angle = Mathf.Abs(Vector3.Angle(this.transform.forward, toEnemy));
+
+                    if (angle < this.halfFieldOfView)
+                    {
+                        this.currentTarget = posibleEnemy;
+                    }
                 }
             }
         }
