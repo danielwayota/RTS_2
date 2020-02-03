ec382f31a936d1794d5794ea9e74b7a2b6a5a4f6
Daniel Wayota <danielglez1990@gmail.com>
2020-02-03 16:44:08 +0000

    [WIP]: Mechanic component

---
+++ Assets/Scripts/Units/Combat/Mechanic.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+
+public class Mechanic : Unit
+{
+    public GameObject healingEffect;
+
+    public float detectionDistance = 1f;
+    public LayerMask unitsLayer;
+
+    public int healingPoints = 5;
+
+    private float time;
+    private float timeOut = 1f;
+
+    private Walk walk;
+
+    // ================================
+    void Awake()
+    {
+        this.walk = GetComponent<Walk>();
+    }
+
+    // ================================
+    void Update()
+    {
+        this.time += Time.deltaTime;
+
+        // Reloj
+        if (this.time > this.timeOut)
+        {
+            this.time = 0;
+
+            // Check near friend units
+
+            var unitHealth = this.GetNearFriendToCure();
+
+            if (unitHealth != null)
+            {
+                unitHealth.health += this.healingPoints;
+
+                Instantiate(this.healingEffect, unitHealth.transform.position, Quaternion.identity);
+            }
+        }
+    }
+
+    /// =================================
+    /// <summary>
+    ///
+    /// </summary>
+    public Health GetNearFriendToCure()
+    {
+        // Comprobar los alrededores.
+        Collider[] nearUnits = Physics.OverlapSphere(
+            this.transform.position,
+            this.detectionDistance,
+            this.unitsLayer
+        );
+
+        float currentMinHealth = float.MaxValue;
+        Health nearFriendUnit = null;
+
+        // Busca el compañero con el menor nivel de salud.
+        for (int i = 0; i < nearUnits.Length; i++)
+        {
+            if (nearUnits[i].gameObject != this.gameObject)
+            {
+                Unit posibleFriend = nearUnits[i].GetComponent<Unit>();
+
+                if (posibleFriend.faction == this.faction)
+                {
+                    Health h = posibleFriend.GetComponent<Health>();
+
+                    if (h.health < currentMinHealth) {
+                        currentMinHealth = h.health;
+
+                        nearFriendUnit = h;
+                    }
+                }
+            }
+        }
+
+        return nearFriendUnit;
+    }
+}
