c8e9a6bef4373a4b1189e497e902b95fe5bd9431
Daniel Wayota <danielglez1990@gmail.com>
2020-04-17 12:33:42 +0100

    Map Orchestra

---
+++ Assets/Scripts/General/Managers/MapOrchestra.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class MapOrchestra : MonoBehaviour
+{
+    public GameObject ui;
+
+    void Awake()
+    {
+        Instantiate(this.ui);
+
+        // TODO: Crear faciones
+        // TODO: Crear bases y asignar faciones
+    }
+}
