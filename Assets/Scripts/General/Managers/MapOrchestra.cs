b9ef83364e466d9a53f6cc755274d66a65caef5a
Daniel Wayota <danielglez1990@gmail.com>
2020-04-27 15:45:26 +0100

    Unit meta storage made prefab

--- Assets/Scripts/General/Managers/MapOrchestra.cs
+++ Assets/Scripts/General/Managers/MapOrchestra.cs
@@ -4,9 +4,13 @@ public class MapOrchestra : MonoBehaviour
 {
     public GameObject ui;
 
+    public GameObject unitMetaStorage;
+
     void Awake()
     {
         Instantiate(this.ui);
+
+        Instantiate(this.unitMetaStorage);
 
         // TODO: Crear faciones
         // TODO: Crear bases y asignar faciones
