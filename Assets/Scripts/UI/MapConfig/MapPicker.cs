ef15bf1dd342f04d6d4b226eeebb2d1305ec0173
Daniel Wayota <danielglez1990@gmail.com>
2020-04-27 11:50:06 +0100

    Map picker

---
+++ Assets/Scripts/UI/MapConfig/MapPicker.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MapPicker : MonoBehaviour
+{
+    [Header("Current maps")]
+    public string[] mapNames;
+
+    [Header("UI")]
+    public Text currentMap;
+    public RectTransform sceneButtonHolders;
+
+    [Header("Prefabs")]
+    public GameObject sceneSelectBtn;
+
+    private string gameMapName;
+
+    /// =========================================
+    /// <summary>
+    ///
+    /// </summary>
+    void Awake()
+    {
+        var randomMapBtn = Instantiate(this.sceneSelectBtn);
+        randomMapBtn.transform.SetParent(this.sceneButtonHolders);
+
+        randomMapBtn.GetComponentInChildren<Text>().text = "(?)";
+
+        randomMapBtn.GetComponent<Button>().onClick.AddListener(() => {
+            int rndIndex = Random.Range(0, this.mapNames.Length);
+
+            int i = 0;
+            while (this.gameMapName == this.mapNames[rndIndex] && i < 100)
+            {
+                rndIndex = Random.Range(0, this.mapNames.Length);
+                i++;
+            }
+
+            this.SetMap(this.mapNames[rndIndex]);
+        });
+
+        int index = Random.Range(0, this.mapNames.Length);
+        this.SetMap(this.mapNames[index]);
+
+        // Generate map buttons
+        foreach (var name in this.mapNames)
+        {
+            var mapBtn = Instantiate(this.sceneSelectBtn);
+            mapBtn.transform.SetParent(this.sceneButtonHolders);
+            mapBtn.GetComponentInChildren<Text>().text = name;
+
+            var btn = mapBtn.GetComponent<Button>();
+
+            var mapName = name + "";
+            btn.onClick.AddListener(() => {
+                this.SetMap(mapName);
+            });
+        }
+    }
+
+    /// =========================================
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="name"></param>
+    private void SetMap(string name)
+    {
+        this.gameMapName = name;
+        this.currentMap.text = name;
+    }
+}
